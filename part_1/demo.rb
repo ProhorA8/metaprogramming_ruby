ef0fcb440523ae19cfad8492d3a34433cf401fce
ProhorA8 <Procustom4@gmail.com>
2022-02-16 23:28:19 +0300

    dynamic declaration methods

--- part_1/demo.rb
+++ part_1/demo.rb
@@ -5,5 +5,20 @@ require_relative 'group'
 album_data = File.read('./data/album.json')
 group_data = File.read('./data/group.json')
 
-puts Oj.load(album_data)
-puts Oj.load(group_data)
+album = Album.new
+
+# первый подход
+# обходим хэш
+# Oj.load(album_data).each do |method, value|
+#   # выполняем присваивание методу
+#   album.send "#{method}=", value
+# end
+
+# второй подход
+Oj.load(album_data).each do |method, value|
+  # заполняем значения переменных образца класса
+  album.instance_variable_set :"@#{method}", value
+end
+
+puts album.inspect #=> #<Album:0x00007fc3ca912090 @title="Ride the Lightning",
+# @group="Metallica", @year=1984, @label="Megaforce Records", @length="00:47:25", @tracks_count=10>
